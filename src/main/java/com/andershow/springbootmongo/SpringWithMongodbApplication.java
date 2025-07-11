2d972b13ff6359647a3cc150f8a7d0310e3f05b7
Anderson Pires <anderson.slg@gmail.com>
2025-07-10 22:56:54 -0300

    first commit

---
+++ src/main/java/com/andershow/springbootmongo/SpringWithMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.andershow.springbootmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andershow/springbootmongo/SpringWithMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andershow.springbootmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
