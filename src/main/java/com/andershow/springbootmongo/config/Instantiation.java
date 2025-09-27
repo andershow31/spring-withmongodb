bdd4423547fa22c96beb3137c3eb436223b8b441
Anderson Pires <148875704+andershow31@users.noreply.github.com>
2025-09-27 13:04:37 -0300

    entidade post com user aninhado

--- src/main/java/com/andershow/springbootmongo/config/Instantiation.java
+++ src/main/java/com/andershow/springbootmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.andershow.springbootmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.andershow.springbootmongo.domain.Post;
 import com.andershow.springbootmongo.domain.User;
+import com.andershow.springbootmongo.repository.PostRepository;
 import com.andershow.springbootmongo.repository.UserRepository;
 
 
@@ -16,16 +20,26 @@ import com.andershow.springbootmongo.repository.UserRepository;
 @Configuration
 public class Instantiation implements CommandLineRunner{
 	@Autowired
+	PostRepository postRepository;
+	@Autowired
 	UserRepository userRepository;
 	@Override
 	public void run(String... args) throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		//o código,as duas linhas, acima define o formato da data, considerando o horário de greenwich
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		//o sfd.parse cria
+		Post post1 = new Post(null, sdf.parse("12/02/2025"), "Partiu Viajar", "Partindo de viagem hoje", maria);
+		Post post2 = new Post(null, sdf.parse("10/03/2025"), "Olá galera", "Este é meu segundo post aqui na rede", maria);
+		postRepository.saveAll(Arrays.asList(post1,post2));		
 	}
 
 }
---
+++ src/main/java/com/andershow/springbootmongo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.andershow.springbootmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.andershow.springbootmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	//a classe mongorepository nos da acesso a várias opções de manipulação de dados dentro do banco
+	//ela exige que passemos como parâmetro a classe a ser manipulada bem como o tipo de dado do id do objeto
+	
+}
